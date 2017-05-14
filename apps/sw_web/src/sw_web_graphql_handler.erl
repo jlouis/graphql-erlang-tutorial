f5e3d2cc4be482624262b7211b101b19d3e1303b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-05-15 01:01:37 +0200

    Handle the transport code partially.
    This handles the code up to the point where we actually call into
    GraphQL proper. Everything isn't done here yet, but good progress is
    being made in the right direction.

--- apps/sw_web/src/sw_web_graphql_handler.erl
+++ apps/sw_web/src/sw_web_graphql_handler.erl
@@ -41,3 +41,60 @@ rest_init(Req, {priv_file, _, _} = PrivFile) ->
 allowed_methods(Req, State) ->
     {[<<"GET">>, <<"POST">>], Req, State}.
 %% end::allowed_methods[]
+
+%% tag::content_types_accepted[]
+content_types_accepted(Req, State) ->
+    {[
+        {{<<"application">>, <<"json">>, []}, from_json}
+    ], Req, State}.
+%% end:content_types_accepted[]
+
+%% tag::content_types_provided[]
+content_types_provided(Req, State) ->
+    {[
+        {{<<"application">>, <<"json">>, []}, to_json},
+        {{<<"text">>, <<"html">>, []}, to_html}
+    ], Req, State}.
+%% end::content_types_provided[]
+
+%% tag::charsets_provided[]
+charsets_provided(Req, State) ->
+    {[<<"utf-8">>], Req, State}.
+%% end::charsets_provided[]
+
+%% tag::resource_exists[]
+resource_exists(Req, #{ method := <<"GET">> } = State) ->
+    {true, Req, State};
+resource_exists(Req, #{ method := <<"POST">> } = State) ->
+    {false, Req, State}.
+%% end::resource_exists[]
+
+%% tag::to_html[]
+to_html(Req, #{ index_location :=
+                    {priv_file, App, FileLocation}} = State) ->
+    Filename = filename:join(code:priv_dir(App), FileLocation),
+    {ok, Data} = file:read_file(Filename),
+    {Data, Req, State}.
+%% end::to_html[]
+
+gather(Req, State) ->
+    {ok, Body, Req2} = cowboy_req:body(Req),
+    {Bindings, Req3} = cowboy_req:bindings(Req2),
+    Params = maps:from_list(Bindings),
+    try jsx:decode(Body, [return_maps]) of
+        JSON ->
+            gather(Req3, State, JSON, Params)
+    catch
+        error:badarg ->
+            {error, invalid_json_body}
+    end.
+
+gather(Req, State, Body, Params) ->
+    QueryDocument = document([Params, Body]),
+    case variables([Params, Body]) of
+        {ok, Vars} ->
+            Operation = operation_name([Params, Body]),
+            {ok, QueryDocument, Vars, Opations};
+        {error, Reason} ->
+            {error, Reason}
+    end.
