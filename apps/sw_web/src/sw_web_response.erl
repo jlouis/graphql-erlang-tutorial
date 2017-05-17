b95698440f5a6b2f104c806fabd977483878246d
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-05-17 13:18:02 +0200

    Fixup keys properly.

--- apps/sw_web/src/sw_web_response.erl
+++ apps/sw_web/src/sw_web_response.erl
@@ -12,11 +12,17 @@ fixup(Term) when is_binary(Term) -> Term;
 fixup(Term) when is_list(Term) ->
     [fixup(T) || T <- Term];
 fixup(Term) when is_map(Term) ->
-    maps:map(fun(_K, V) ->
-                     fixup(V)
-             end, Term);
+    KVs = maps:to_list(Term),
+    maps:from_list([{fixup_key(K), fixup(V)} || {K, V} <- KVs]);
 fixup(Term) ->
     %% Every other term is transformed into a binary value
     iolist_to_binary(
       io_lib:format("~p", [Term])).
 
+fixup_key(Term) ->
+    case fixup(Term) of
+        T when is_binary(T) ->
+            T;
+        T ->
+            iolist_to_binary(io_lib:format("~p", [T]))
+    end.
