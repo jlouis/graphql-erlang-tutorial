539d7d7170ed9f885e1bea5003c0ea7ae85d44bb
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-05-23 23:15:26 +0200

    Correctly handle the load of starships and vehicles

--- apps/sw_core/src/sw_core_film.erl
+++ apps/sw_core/src/sw_core_film.erl
@@ -1,5 +1,6 @@
 -module(sw_core_film).
 -include("sw_core_db.hrl").
+-include_lib("stdlib/include/qlc.hrl").
 
 -export([execute/4]).
 
@@ -40,17 +41,29 @@ execute(_Ctx, #film{} = Film, Field, Args) ->
         <<"starshipConnection">> ->
             #film { starships = Starships } = Film,
             Txn = fun() ->
-                          [mnesia:read(species, S) || S <- Starships]
+                          QH = qlc:q([#{ starship => Ss,
+                                         transport => Tspt } ||
+                                         Ss <- mnesia:table(starship),
+                                         Tspt <- mnesia:table(transport),
+                                         Ss#starship.id == Tspt#transport.id,
+                                         lists:member(Ss#starship.id, Starships)]),
+                          qlc:e(QH)
                   end,
             {atomic, Records} = mnesia:transaction(Txn),
-            sw_core_paginate:select(lists:append(Records), Args);
+            sw_core_paginate:select(Records, Args);
         <<"vehicleConnection">> ->
             #film { vehicles = Vehicles } = Film,
             Txn = fun() ->
-                          [mnesia:read(species, V) || V <- Vehicles]
+                          QH = qlc:q([#{ vehicle => V,
+                                         transport => Tspt } ||
+                                         V <- mnesia:table(vehicle),
+                                         Tspt <- mnesia:table(transport),
+                                         V#vehicle.id == Tspt#transport.id,
+                                         lists:member(V#vehicle.id, Vehicles)]),
+                          qlc:e(QH)
                   end,
             {atomic, Records} = mnesia:transaction(Txn),
-            sw_core_paginate:select(lists:append(Records), Args)
+            sw_core_paginate:select(Records, Args)
     end.
 %% end::filmExecute[]
 
