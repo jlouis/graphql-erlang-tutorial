19f0ca1d9cd5df59f08effaf2a22d3c6dcc66c2b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2017-05-30 12:59:56 +0200

    Introduction code for starships

--- apps/sw_core/src/sw_core_starship.erl
+++ apps/sw_core/src/sw_core_starship.erl
@@ -3,6 +3,7 @@
 -include_lib("stdlib/include/qlc.hrl").
 
 -export([execute/4]).
+-export([introduce/2]).
 
 %% tag::starshipExecute[]
 execute(_Ctx, #{ starship := #starship { id = StarshipId } = Starship,
@@ -50,5 +51,50 @@ execute(_Ctx, #{ starship := #starship { id = StarshipId } = Starship,
             sw_core_paginate:select(Records, Args)
     end.
 
+%% tag::introduce[]
+introduce(_Ctx, #{ <<"name">> := Name,
+                   <<"model">> := Model,
+                   <<"starshipClass">> := Class,
+                   <<"manufacturers">> := Manufacturers,
+                   <<"costInCredits">> := Cost,
+                   <<"length">> := Length,
+                   <<"crew">> := Crew,
+
+                   <<"faction">> := FactionID }) ->
+    ID = sw_core_db:nextval(transport),
+    Transport = #transport { id = ID,
+                             name = Name,
+                             created = current_time(),
+                             edited = current_time(),
+                             crew = Crew,
+                             model = Model,
+                             cost = Cost,
+                             length = Length,
+                             manufacturers = Manufacturers },
+    Starship = #starship { id = ID,
+                           starship_class = Class },
+    case sw_core_db:load('Faction', FactionID) of
+        {ok, #faction { id = FactionRef } = Faction} ->
+            Txn = fun() ->
+                          ok = mnesia:write(Starship),
+                          ok = mnesia:write(Transport#transport {
+                                              faction = FactionRef
+                                             }),
+                          ok
+                  end,
+            {atomic, ok} = mnesia:transaction(Txn),
+            {ok, Faction, #{ starship => Starship,
+                             transport => Transport#transport {
+                                            faction = FactionRef
+                                           }}};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+                           
+%% end::introduce[]
 floatify(nan) -> null;
 floatify(I) -> float(I).
+
+current_time() ->
+    calendar:universal_time().
+
